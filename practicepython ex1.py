1057d68a6ca0022a89bf3798993df8862179b012
kaithehungrycat <60157330+kaithehungrycat@users.noreply.github.com>
2020-02-17 18:12:01 +0100

    Initial commit

---
+++ practicepython ex1.py
@@ -0,0 +1,10 @@
+from datetime import *
+
+a = input("Hey Mate! Please tell me your name: ")
+b = int(input("And how old are you? "))
+c = int(input("How many copies would you like to see?"))
+d = datetime.now()
+
+while c > 0:
+    print("Hey {}! Did you know that you will turn 100 in year {}?".format(a, (d.year - b) + 100))
+    c -= 1
