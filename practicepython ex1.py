82ae161555f05e1485e8ff6e94276a5c4f79c18f
kaithehungrycat <lukasz.dawid@hotmail.com>
2020-02-18 20:04:44 +0100

    Initial commit

--- practicepython ex1.py
+++ practicepython ex1.py
@@ -6,5 +6,5 @@ c = int(input("How many copies would you like to see?"))
 d = datetime.now()
 
 while c > 0:
-    print("Hey {}! Did you know that you will turn 100 in year {}?".format(a, (d.year - b) + 100))
+    print("Hey {}! \nDid you know that you will turn 100 in year {}?".format(a, (d.year - b) + 100))
     c -= 1
