79515f92d1266f2217b24ef0e05afedcb60ad2a4
kaithehungrycat <lukasz.dawid@hotmail.com>
2020-02-23 16:56:36 +0100

    Initial commit

--- practicepython ex5.py
+++ practicepython ex5.py
@@ -1,5 +1,5 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] #lista a
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 55, 89, 55] #lista b
+b = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 55, 89, 55] #lista b
 c = [] #porownanie a do b
 d = [] #porowbanie b do a
 
@@ -11,5 +11,8 @@ for element in b:
     if element in a:
         d.append(element)
 
-print(list(dict.fromkeys(c))) #wynik z usunietymi duplikatami
+
+c=set(c)
+
+print(c) #wynik z usunietymi duplikatami
 print(list(dict.fromkeys(c))) #wynik z usunietymi duplikatami
