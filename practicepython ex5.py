01a3beac430f44614137d252b3f531bb55aa8a61
kaithehungrycat <lukasz.dawid@hotmail.com>
2020-02-23 16:59:17 +0100

    Initial commit

--- practicepython ex5.py
+++ practicepython ex5.py
@@ -11,8 +11,24 @@ for element in b:
     if element in a:
         d.append(element)
 
-
 c=set(c)
 
 print(c) #wynik z usunietymi duplikatami
 print(list(dict.fromkeys(c))) #wynik z usunietymi duplikatami
+
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 55, 89, 55]
+f = [0, 34]
+c = []
+d = []
+
+e = [element for element in a if element in b]
+g = [element for element in a if element in f]
+
+e = set(e)
+g = set(g)
+
+print(e)
+print(g)
