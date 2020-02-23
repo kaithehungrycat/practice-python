b103baeb6f3bd3e8932f84d7b28ac9ad8789a471
kaithehungrycat <60157330+kaithehungrycat@users.noreply.github.com>
2020-02-23 15:08:13 +0100

    Initial commit

--- Test2.py
+++ Test2.py
@@ -1,3 +1,6 @@
-input_num = int(input("choose a number: "))
-lista = list(range(1, input_num+1))
-print([element for element in lista if input_num % element == 0])
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 55, 89, 55]
+c = []
+d = []
+
+print(c.append(a) for element in a if element in a == b)
---
+++ practicepython ex5.py
@@ -0,0 +1,15 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] #lista a
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 55, 89, 55] #lista b
+c = [] #porownanie a do b
+d = [] #porowbanie b do a
+
+for element in a:
+    if element in b:
+        c.append(element)
+
+for element in b:
+    if element in a:
+        d.append(element)
+
+print(list(dict.fromkeys(c))) #wynik z usunietymi duplikatami
+print(list(dict.fromkeys(c))) #wynik z usunietymi duplikatami
