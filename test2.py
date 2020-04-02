792b79329498dadb633b66871e1a017472d44cfb
kaithehungrycat <lukasz.dawid@hotmail.com>
2020-04-02 23:49:09 +0200

    ex7

--- test2.py
+++ test2.py
@@ -5,12 +5,23 @@ u2 = int(input("Rock, Scissors or Paper. What do you choose?"))
 
 
 def compare(a, b):
+    range(0, 2)
     if a == b:
         print("Tie")
-    elif a-b == 1 or a-b == -2:
+    elif a - b == 1 or a - b == -2:
         print("Win A")
-    else:
+    elif a - b == -1 or a - b == 2:
         print("Win B")
+    else:
+        print("Invalid input")
 
 
 print(compare(u1, u2))
+
+one_more_time = str(input("Once more? [y/n]"))
+
+while one_more_time == "y":
+    u1 = int(input("Rock, Scissors or Paper. What do you choose?"))
+    u2 = int(input("Rock, Scissors or Paper. What do you choose?"))
+    print(compare(u1, u2))
+    one_more_time = str(input("Once more? [y/n]"))
