bb96c367ebd3f0f4b7063b7009965a1b0f62ef58
kaithehungrycat <lukasz.dawid@hotmail.com>
2020-04-02 23:35:05 +0200

    ex7

--- test2.py
+++ test2.py
@@ -1,18 +1,16 @@
-0 = kamien
-1 = papier
-2 = nozyce
-
-
 print("Hey! to play use: \n 0 - kamien \n 1 - papier \n 2 - nozyce")
 
-user1 = str(input("Rock, Scissors or Paper. What do you choose?"))
-user2 = str(input("Rock, Scissors or Paper. What do you choose?"))
+u1 = int(input("Rock, Scissors or Paper. What do you choose?"))
+u2 = int(input("Rock, Scissors or Paper. What do you choose?"))
 
-def compare(a,b):
+
+def compare(a, b):
     if a == b:
         print("Tie")
-    elif a - b == 1 or a - b == -2:
+    elif a-b == 1 or a-b == -2:
         print("Win A")
     else:
         print("Win B")
 
+
+print(compare(u1, u2))
