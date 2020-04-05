ad7c63377960390a2f0f5507ba96651fb5513f17
kaithehungrycat <lukasz.dawid@hotmail.com>
2020-04-05 19:00:07 +0200

    ex7

--- practicepython ex8.1.py
+++ practicepython ex8.1.py
@@ -6,16 +6,17 @@ u2 = int(input("Rock, Scissors or Paper. What do you choose?"))
 
 def compare(a, b):
     if a == b in range(0, 3):
-        print("Tie")
+        return "Tie"
     elif a - b == 1 or a - b == -2 in range(0, 3):
-        print("Win A")
+        return "Win A"
     elif a - b == -1 or a - b == 2 in range(0, 3):
-        print("Win B")
+        return "Win B"
     else:
-        print("Invalid input")
+        return "Invalid input"
 
 
 print(compare(u1, u2))
+
 
 one_more_time = str(input("Once more? [y/n]"))
 
