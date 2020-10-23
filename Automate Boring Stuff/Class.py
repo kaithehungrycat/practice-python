37535e21d0c4daae4a2ca1792bab81a566235ebb
lukas <lukasz,.dawid@hotmail.com>
2020-10-23 17:34:45 +0200

    commit kjhoikl

---
+++ Automate Boring Stuff/Class.py
@@ -0,0 +1,18 @@
+import xlwings as xl
+
+wb = xl.books.add(r'C:\Users\lukas\Desktop\test.xlsx')
+
+# class Test:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def introduce(self, welcome="Hey"):
+#         return welcome + " my name is " + self.name + " age " + str(self.age)
+#
+#
+# obj1 = Test("Luke", 29)
+# obj2 = Test("Agata", 29)
+# print(obj1.name)
+# print(obj1.age)
+# print(obj1.introduce("welcome"))
